dfe79c0d6205c004ac45a3fd61299c2b24891930
Kushagradhi Bhowmik <rivu.ilsssd@gmail.com>
2019-04-14 23:48:20 -0500

    sub category

--- code/sb.py
+++ code/sb.py
@@ -1,5 +1,7 @@
 from nltk.parse import CoreNLPParser
 from nltk.corpus import wordnet as wn
+from nltk.corpus import stopwords 
+from nltk.tokenize import word_tokenize 
 from enum import Enum
 
 class DB(Enum):
@@ -30,30 +32,31 @@ def hasOnlyLocationTags(sentences):
     return tags
 
 def getCategoryPredictions(sentences):
-    category = ['movie', 'music']
-    # category = ['geography', 'movie', 'music']
-    # list_sentences=['Which pop artist sings CrazyInLove?', 
-    #                 'Which is the deepest ocean?']
-    # a='Did Neeson star in Schindler’sList'#.lower()
+    # category = ['movie', 'music']
+    # category = ['geographical', 'cinema', 'music']
+    category = [['place', 'geographic', 'mountain', 'ocean'], ['cinema', 'director', 'oscar', 'movie', 'actor'], ['Gaga', 'music', 'musician', 'sing', 'album']]
     tags=[]
     for sentence in sentences:
         scores=[]
-        for i in category:
-            score = 0
-            cat_synset = wn.synsets(i)[0]
-            for word in sentence.split():
-                word_synsets = wn.synsets(word)
-                if(len(word_synsets)==0):
-                    continue
-                word_synset = wn.synsets(word)[0]
-                # print(f'{word}: {word_synset}')
-                if(word_synset.path_similarity(cat_synset) != None):
-                    score += word_synset.path_similarity(cat_synset)
-                # if(word_synset.lch_similarity(cat_synset) != None):
-                #     score += word_synset.lch_similarity(cat_synset)
-                # print(f'{word}: {score}')
-            scores.append(score)
-        tags.append(1+scores.index(max(scores)))
+        for cat_i in category:
+            for i in cat_i:
+                score = 0
+                cat_synset = wn.synsets(i)[0]
+                for word in sentence.split():
+                    word_synsets = wn.synsets(word)
+                    if(len(word_synsets)==0):
+                        continue
+                    word_synset = wn.synsets(word)[0]
+                    # print(f'{word}: {word_synset}')
+                    if(word_synset.path_similarity(cat_synset) != None):
+                        # score.append(word_synset.path_similarity(cat_synset))
+                        score += word_synset.path_similarity(cat_synset)
+                    # if(word_synset.lch_similarity(cat_synset) != None):
+                    #     score += word_synset.lch_similarity(cat_synset)
+                    # print(f'{word}: {score}')
+            # scores.append(max(score))
+            scores.append(score/len(i))
+        tags.append(scores.index(max(scores)))
     return tags
     # for i in range(len(category)):
     #     print(f'{category[i]}: {scores[i]}', end="\t")
@@ -64,12 +67,21 @@ def main():
         queries = f.readlines()
     # print(queries)
     tags = hasOnlyLocationTags(queries)
+    stop_words = set(stopwords.words('english')) 
+    for i, query in enumerate(queries):
+        queries[i] = " ".join([word for word in word_tokenize(query) if not word in stop_words])
+    # print(queries)
     nonGeoIndices = [i for i,el in enumerate(tags) if el==-1]
     tagsOthers = getCategoryPredictions([queries[i] for i in nonGeoIndices])
     for i in range(len(tagsOthers)):
         tags[nonGeoIndices[i]] = tagsOthers[i]
     
     for i in range(len(tags)):
-        print(f'{DB(tags[i]).name}: {queries[i]}')
+        if i in nonGeoIndices:
+            print(f'{DB(tags[i]).name}: {queries[i]}')
+    print("\n\n From NER")
+    for i in range(len(tags)):
+        if i not in nonGeoIndices:
+            print(f'{DB(tags[i]).name}: {queries[i]}')
 
 main()
